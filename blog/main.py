49a488011acf936aecffec3110391a999c114165
Your Name <ramendra@dozee.io>
2022-05-16 09:24:43 +0530

    addded user and refactored the code

--- blog/main.py
+++ blog/main.py
@@ -1,65 +1,20 @@
-from fastapi import FastAPI, Depends, status, Response, HTTPException
+from fastapi import FastAPI
 from . import schemas, models
-from .database import engine, SessionLocal
-from sqlalchemy.orm import Session
+from .database import engine
 import uvicorn
+
+from .router import blog, user
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog,
-           db: Session =Depends(get_db) ):
-    new_blog = models.Blog(title= request.title,
-                           body = request.body)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
-
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED  )
-def update(id, request: schemas.Blog, db: Session =Depends(get_db)):
-    print(request.dict())
-    print(dict(request))
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not  blog.first():
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail= f'Blog with {id} not found')
-    blog.update(dict(request))
-    db.commit()
-    return "UPDATED SUCCESSFULLY"
+app.include_router(blog.router)
+app.include_router(user.router)
 
 
-@app.get('/blog')
-def all(db: Session =Depends(get_db) ):
-    blogs = db.query(models.Blog).all()
-    return blogs
 
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id,db: Session =Depends(get_db)  ):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not  blog.first():
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail= f'Blog with {id} not found')
-    blog.delete(synchronize_session=False)
-    db.commit()
-    return "done"
 
-@app.get('/blog/{id}')
-def show(id, response:Response,  db: Session =Depends(get_db) , status_code = 200):
-     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-     if not blog:
-         # response.status_code = status.HTTP_404_NOT_FOUND
-         # return {'detail': f"Blog with {id} is not available"}
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail =f"Blog with {id} is not available")
-     return blog
+
 
 # if __name__ == '__main__':
 #     uvicorn.run(app, host = "127.0.0.1", port = 9000)
